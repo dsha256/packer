eec67ee147c4b126f6722d4479d25b361625a88a
David Shainidze <56347073+dsha256@users.noreply.github.com>
2023-11-20 01:08:41 +0400

    feat/packer: add packer (#10)
    * chore: add packer
    
    * test(packer): unit tests
    
    * chore(sec): improve code security

--- internal/packer/packer.go
+++ internal/packer/packer.go
@@ -1,4 +1,14 @@
 package packer
 
+import "context"
+
 type Sizer interface {
+	AddSize(ctx context.Context, sizeToAdd int) ([]int, error)
+	PutSizes(ctx context.Context, sizesToPut []int) ([]int, error)
+	DeleteSize(ctx context.Context, sizeToDelete int) ([]int, error)
+	Exists(sizeToCheckFor int) bool
+}
+
+type Packer interface {
+	GetPackets(ctx context.Context, itemsToPack int) (map[int]int, error)
 }
